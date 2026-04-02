09ee777f55f79f092ef6a6ab32ff8cd9b844e38a
eijirou <sacjapan@outlook.com>
2026-04-02 17:03:09 +0900

    新規プロジェクト

---
+++ BudgetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by 野澤英二郎 on 2026/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
