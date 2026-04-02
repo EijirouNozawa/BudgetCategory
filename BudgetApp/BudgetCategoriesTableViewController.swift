f4966bb821b1706509052b2dbab3efef4f6fdb69
eijirou <sacjapan@outlook.com>
2026-04-02 18:31:18 +0900

    カテゴリーボタンを追加

--- BudgetApp/BudgetCategoriesTableViewController.swift
+++ BudgetApp/BudgetCategoriesTableViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import CoreData
 
 class BudgetCategoriesTableViewController: UITableViewController {
-
+    
+    private var persistentContainer: NSPersistentContainer!
+    
+    init(persistentContainer: NSPersistentContainer) {
+        self.persistentContainer = persistentContainer
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setuiUI()
+    }
+    
+    @objc func showAddBudgetCategory(_ sender: UIBarButtonItem) {
+        
+        
     }
 
+    private func setuiUI() {
+        
+        let addBudgetCategoryButton = UIBarButtonItem(title: "カテゴリーを追加", style: .done, target: self, action: #selector(showAddBudgetCategory))
+        self.navigationItem.rightBarButtonItem = addBudgetCategoryButton
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "予算"
+    }
 
 }
 
--- BudgetApp/SceneDelegate.swift
+++ BudgetApp/SceneDelegate.swift
@@ -6,18 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BudgetModel")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
       guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = BudgetCategoriesTableViewController()
         window.makeKeyAndVisible()
+        let navigationController = UINavigationController(rootViewController: BudgetCategoriesTableViewController(persistentContainer: persistentContainer))
+        window.rootViewController = navigationController
         self.window = window
     }
 
