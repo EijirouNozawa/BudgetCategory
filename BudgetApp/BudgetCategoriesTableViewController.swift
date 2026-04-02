aff5c6d8e3719cf56044c6014392d308d6d8060a
eijirou <sacjapan@outlook.com>
2026-04-02 17:50:25 +0900

    make DataModel

--- BudgetApp/BudgetCategoriesTableViewController.swift
+++ BudgetApp/BudgetCategoriesTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BudgetCategoriesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
